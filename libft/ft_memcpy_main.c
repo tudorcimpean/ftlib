42b1823078a192876c40bc33421f4dc36c54f65a
Tudor CIMPEAN <tcimpean@e1p64.cluj.42.fr>
2015-10-26 23:16:16 +0100

    memccpy + memcpy main

--- libft/ft_memcpy_main.c
+++ libft/ft_memcpy_main.c
@@ -6,7 +6,7 @@
 /*   By: tcimpean <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/10/26 22:35:49 by tcimpean          #+#    #+#             */
-/*   Updated: 2015/10/26 22:54:05 by tcimpean         ###   ########.fr       */
+/*   Updated: 2015/10/26 23:12:44 by tcimpean         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,15 @@
 #include <string.h>
 
 void    *ft_memcpy(void *dst, const void *src, size_t n);
+void    *ft_memccpy(void *dst, const void *src,  int c, size_t n);
 
 int		main (void)
 {
-	char src[50] = "http://www.tutorialspoint.com";
-	char dest[70] = "adsasdaasdfggfdfghfgddfgdfgdfgasdadsadsasdadsadsads";
+	char src[10] = "abcAdefgh";
+	char dest[10] = "123456789";
 
 	printf("Before memcpy dest = %s\n", dest);
-	ft_memcpy(dest, src, strlen(src));
+	memccpy(dest, src, 65, 6);
 	printf("After memcpy dest = %s\n", dest);
 
 	return(0);
